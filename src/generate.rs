3c63e0cdfc8adeae4948101c78f9463511263c20
parkcitymedia <blakemichaelgaynor@gmail.com>
2021-07-27 11:33:30 -0700

    un-hide generator funcs \[broken]

--- src/generate.rs
+++ src/generate.rs
@@ -1,39 +1,43 @@
 use std::io::{stdin,stdout,Write};
 
-    /*
-    /// generate a polygon of n (`side_input`) sides
-    pub fn custom_polygon() {
-        let mut side_input: i32;
-        println!("number of sides:");
-        stdin().read_line(&mut side_input).expect("unicode is pretty diverse so you have to have entered in something REALLY out-of-context here.");
+// include main in this so we
+// can borrow stuff like tile
+/// structure and whatnot
+mod lib;
+/*
+/// generate a polygon of n (`side_input`) sides
+pub fn custom_polygon() {
+    let mut side_input: i32;
+    println!("number of sides:");
+    stdin().read_line(&mut side_input).expect("unicode is pretty diverse so you have to have entered in something REALLY out-of-context here.");
 
-        
-
-    }
-    */
-
-    /*
-    pub fn triangle() {
-        unimplemented!();
-    }
-
-    pub fn quadagon() {
-        unimplemented!();
-    }
-
-    pub fn pentagon() {
-        unimplemented!();
-    }
-
-    pub fn hexagon() {
-        unimplemented!();
-    }
-
-    pub fn septagon() {
-        unimplemented!();
-    }
     
-    pub fn octagon() {
-        unimplemented!();
-    }
-    */
+
+}
+*/
+
+/// # generate::triangle()
+/// outputs a ute-style json "triangle"
+/// polygon.
+pub fn triangle() {
+    unimplemented!();
+}
+
+pub fn quadagon() {
+}
+
+pub fn pentagon() {
+    unimplemented!();
+}
+
+pub fn hexagon() {
+    unimplemented!();
+}
+
+pub fn septagon() {
+    unimplemented!();
+}
+
+pub fn octagon() {
+    unimplemented!();
+}
