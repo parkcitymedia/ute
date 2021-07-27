93f8cb12f357d34656e0a9e6ccee2515ba8b27da
parkcitymedia <blakemichaelgaynor@gmail.com>
2021-07-27 11:19:36 -0700

    separate generation tool into lib \[broken]

--- src/lib.rs
+++ src/lib.rs
@@ -80,44 +80,5 @@ pub fn identify_tile(tile: &mut Tile) -> Result<Tile, Box<dyn std::error::Error>
 }
 
 /// tile generation tools.
-pub mod generate {
-
-    use std::io::{stdin,stdout,Write};
-
-    /// generate a polygon of n (`side_input`) sides
-    pub fn custom_polygon() {
-        let mut side_input: String = String::new();
-        println!("number of sides:");
-        stdin().read_line(&mut side_input).expect("unicode is pretty diverse so you have to have entered in something REALLY out-of-context here.");
-
-        
-
-    }
-
-    /*
-    pub fn triangle() {
-        unimplemented!();
-    }
-
-    pub fn quadagon() {
-        unimplemented!();
-    }
-
-    pub fn pentagon() {
-        unimplemented!();
-    }
-
-    pub fn hexagon() {
-        unimplemented!();
-    }
-
-    pub fn septagon() {
-        unimplemented!();
-    }
-    
-    pub fn octagon() {
-        unimplemented!();
-    }
-    */
-
-}
+#[cfg(feature="generate")]
+pub mod generate;
